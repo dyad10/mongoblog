4c128b284146bd413049dbe688b4e81a638435d7
Andy Chen <andyfchen@gmail.com>
2012-05-15 10:11:46 -0400

    added in memory datastore

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
+var ArticleProvider = require('../articleprovider-memory').ArticleProvider;
+
+var articleProvider = new ArticleProvider();
 
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' })
+  articleProvider.findAll(function(error, docs) {
+    res.render('index', { title: 'Blog', articles: docs });
+  });
+//  res.render('index', { title: 'Express' })
 };
